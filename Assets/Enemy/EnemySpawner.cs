2a731f879ad5c41a88b8e8b410ae570a12a08993
ablb12 <ablb12@aim.omaha.lib.ne.us>
2018-08-11 12:13:30 -0500

    Made spawn enemies and you win screen

---
+++ Assets/Enemy/EnemySpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemy;
+    public float spawnTime = 5f;
+    public Transform spawnpoint;
+
+    void Start()
+    {
+       InvokeRepeating("Spawn", spawnTime, spawnTime); 
+    }
+    
+
+    public void Spawn()
+    {
+        Instantiate(enemy, spawnpoint.position, Quaternion.identity);
+    }
+}
